0475d8c73d9273bccb57cbea8ead9f4dbe38d4c2
Specialistik <test04@list.ru>
2020-12-20 05:47:21 +0300

    login page

--- my_internet_django/settings.py
+++ my_internet_django/settings.py
@@ -16,7 +16,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
@@ -28,6 +28,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '194.58.122.233']
 
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 
 # Application definition
 
@@ -124,6 +126,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
 APPEND_SLASH = False
