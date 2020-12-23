7d631e9759e8d2469aa3de3d22b86f8f87229ddf
Specialistik <test04@list.ru>
2020-12-23 13:12:17 +0300

    mobile view

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,10 @@
 import React,{Component} from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-//import Login from './login.jsx';
 import './App.css';
-// import { PrivateRoute } from "./PrivateRoute.jsx";
-// import {isLoggedIn} from './auth.js';
 
+/*
 const Home = ()=> <div className="content">
                     <div className="person_info">
-                      {/*<p>Профиль: {localStorage.getItem("fio")}</p>*/}
                       <p>Баланс: 200 р.</p>
                       <p>Дата оплаты: 31.12.2020</p>
                       <p>Сумма к оплате: 300 р.</p>
@@ -24,25 +21,54 @@ const Home = ()=> <div className="content">
                       </div>
                     </div>
                 </div>
+*/
 
-export default class App extends Component{
-  constructor(props){
+class Home extends Component {
+  constructor(props) {
     super(props);
-
+    this.toggleCardInfo = this.toggleCardInfo.bind(this);
     this.state = {
-      access_token:"",
-      fio:""
-    }
+      hidden: false,
+    };
   }
 
+  toggleCardInfo() {
+    const currentState = this.state.hidden;
+    this.setState({ hidden: !currentState });
+  }
+
+  render() {
+    return (
+      <div className="content">
+        <div className="person_info">
+          <p><span class = "label label-warning">Баланс: 200 р.</span></p>
+          <p><span class = "label label-warning">Дата оплаты: 31.12.2020</span></p>
+          <p><span class = "label label-warning">Сумма к оплате: 300 р.</span></p>
+          <button className="btn btn-danger btn-pay" onClick={this.toggleCardInfo}>
+            Пополнить
+          </button>
+        </div>
+        <div className="card_info {this.state.hidden ? 'hidden': null}">
+          <div className="input-group mb-3 row">
+            <input type="text" className="form-control col-md-12" placeholder="Номер карты"/>
+          </div>
+          <div className="input-group mb-3 row">
+            <input type="text" className="form-control col-md-5" placeholder="Срок действия"/>
+            <div className="col-md-1"></div>
+            <input type="text" className="form-control col-md-5" placeholder="CVV/CVC"/>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default class App extends Component{
   render(){
     return(
       <Router>
         <div>
           <Route exact path="/" component={Home} />
-          {/*<Route exact path="/login" component={Login} />
-          }PrivateRoute exact isloggedin={isLoggedIn()} path="/" component={Home} />
-          <Route exact path="/login" component={Login} /> */}
         </div>
     </Router>
       )
