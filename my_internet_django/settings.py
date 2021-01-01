6bf3c1cd52d5b9402da990377cbfd7ca2c54373a
Specialistik <test04@list.ru>
2021-01-02 02:10:05 +0300

    CORS issues

--- my_internet_django/settings.py
+++ my_internet_django/settings.py
@@ -40,12 +40,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'core',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -125,3 +127,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+
+CORS_ALLOWED_ORIGINS = [
+    'http://127.0.0.1',
+    'http://localhost',
+    'http://194.58.122.233',
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    'http://127.0.0.1',
+    'http://localhost',
+    'http://194.58.122.233',
+]
